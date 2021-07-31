4d600d1be2de6bfab9e3384af892bd2ac072f9a0
Shantel <snchristian8@gmail.com>
2021-07-31 14:26:08 -0400

    added updated script.js, added updated index.html

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Let's begin an amazing adventure";
 document.querySelector("body").appendChild(h2); 
